baf9dea4f845de14d20d359b58dd7991b3974deb
= <=>
2014-11-21 17:28:23 -0200

    Adicionado login e listagem .mustache

--- src/main/java/com/ilegra/jt/lancamentodehoras/Routes.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/Routes.java
@@ -1,34 +1,34 @@
 package com.ilegra.jt.lancamentodehoras;
 
 
-import java.util.HashMap;
-import java.util.Map;
 import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
-public class Routes 
-{
-    public static void main( String[] args )
-    {     
+public class Routes {
+    
+    public static void main( String[] args ){     
         
+        
+        // hello.mustache file is in resources/templates directory
+        get("/login", (rq, rs) -> new ModelAndView(null, "login.mustache"), new MustacheTemplateEngine());
+        
+        get("/lancamentohoras", (rq, rs) -> new ModelAndView(null, "listagem.mustache"), new MustacheTemplateEngine());
+        
+        post("/login", (request, response) -> {
+            
+            String usuario = request.queryParams("usuario");
+            String senha = request.queryParams("senha");
+            
+            response.redirect("/lancamentohoras");
+            return String.format("Login: %s <br />Senha: %s", usuario, senha);
+        });
+        
+        
+        /**
 	get("/novo", (request, response) -> "Tela de Login");
         
         get("/listagem", (request, response) -> "Tela de Login");
-        
-        
-        
-        //get("/m", (request, response) -> new ModelAndView(null, "hello.mustache"), new MustacheTemplateEngine());
-
-        Map map = new HashMap();
-        map.put("name", "Sam");
-
-        // hello.mustache file is in resources/templates directory
-        get("/edit", (rq, rs) -> new ModelAndView(map, "hello.mustache"), new MustacheTemplateEngine());
-
-	//get("/marcelo", (req, res) -> "My Name is Marcelo");
-	//get("/paulo", (req, res) -> "My Name is Paulo");
-	//get("/aline", (req, res) -> "My Name is Aline");
-
+        */
     }
 }
