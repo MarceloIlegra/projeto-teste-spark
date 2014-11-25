f3e4f4d118805d9c9f1691fdfb81b4dcb58c2e0c
= <=>
2014-11-25 17:43:05 -0200

    Terminado POGO Project

--- src/main/java/com/ilegra/jt/lancamentodehoras/model/Project.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/model/Project.java
@@ -1,5 +1,38 @@
 package com.ilegra.jt.lancamentodehoras.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Project {
+    
+    private Integer id;
+    private String description;
+    private List<SubProject> subProjects = new ArrayList<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }    
+
+    public List<SubProject> getSubProjects() {
+        return subProjects;
+    }
+
+    public void setSubProjects(List<SubProject> subProjects) {
+        this.subProjects = subProjects;
+    }
+    
+    
     
 }
