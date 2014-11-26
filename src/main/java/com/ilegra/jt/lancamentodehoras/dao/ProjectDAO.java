c141174163bd6ec5019cdbd453868f5852b5892c
= <=>
2014-11-26 14:41:58 -0200

    Implementar interfaces do projeto - Projects

---
+++ src/main/java/com/ilegra/jt/lancamentodehoras/dao/ProjectDAO.java
@@ -0,0 +1,17 @@
+
+package com.ilegra.jt.lancamentodehoras.dao;
+
+import com.ilegra.jt.lancamentodehoras.model.Project;
+import com.ilegra.jt.lancamentodehoras.model.User;
+import com.ilegra.jt.lancamentodehoras.repository.ProjectRepository;
+import java.util.List;
+
+
+public class ProjectDAO implements ProjectRepository{
+
+    @Override
+    public List<Project> listByUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/main/java/com/ilegra/jt/lancamentodehoras/model/Activity.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/model/Activity.java
@@ -2,10 +2,8 @@ package com.ilegra.jt.lancamentodehoras.model;
 
 import java.time.LocalDateTime;
 
-/**
- *
- * @author ilegra0000190
- */
 public class Activity {
+    
+    
     
 }
