c3d46fb503c5344a722fec6e1ec143e24299e8fd
PauloMonteiro02 <paulo_monteiro@ilegra.com>
2014-11-26 14:52:46 -0200

    subproject

---
+++ src/main/java/com/ilegra/jt/lancamentodehoras/dao/SubProjectDAO.java
@@ -0,0 +1,17 @@
+package com.ilegra.jt.lancamentodehoras.dao;
+
+import com.ilegra.jt.lancamentodehoras.main.App;
+import com.ilegra.jt.lancamentodehoras.model.Project;
+import com.ilegra.jt.lancamentodehoras.model.SubProject;
+import com.ilegra.jt.lancamentodehoras.model.User;
+import com.ilegra.jt.lancamentodehoras.repository.SubProjectRepository;
+import java.util.List;
+
+public class SubProjectDAO implements SubProjectRepository{
+
+    @Override
+    public List<SubProject> listByUserAndProject(User user, Project project) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
