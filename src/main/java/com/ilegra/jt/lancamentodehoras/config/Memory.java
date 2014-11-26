94ce854ecd2dcba5364a6fee8eef80a1ab738e2f
PauloMonteiro02 <paulo_monteiro@ilegra.com>
2014-11-26 16:11:54 -0200

    Criar simulacao de banco , criar Array de Projeto e SubProjeto

--- src/main/java/com/ilegra/jt/lancamentodehoras/config/Memory.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/config/Memory.java
@@ -1,4 +1,3 @@
-
 package com.ilegra.jt.lancamentodehoras.config;
 
 import com.ilegra.jt.lancamentodehoras.model.Activity;
@@ -7,46 +6,77 @@ import com.ilegra.jt.lancamentodehoras.model.SubProject;
 import com.ilegra.jt.lancamentodehoras.model.User;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.List;
 import java.util.ArrayList;
 
 public class Memory {
 
-    public static ArrayList<User> users = new ArrayList<User>();
-    public static ArrayList<Activity> activities = new ArrayList<Activity>();
-    
-    public static void start(){
+    public static List<User> users = new ArrayList<User>();
+    public static List<Activity> activities = new ArrayList<Activity>();
+    public static List<Project> projects = new ArrayList<Project>();
+    public static List<SubProject> subProjects = new ArrayList<SubProject>();
+
+    public static void start() {
         users.add(new User("marcelo", "123"));
         users.add(new User("admin", "123"));
-        
-        Project project = new Project();
-        project.setId(0);
-        project.setDescription("Projeto descricao");
-        
-        SubProject subProject = new SubProject();
-        subProject.setId(0);
-        subProject.setDescription("SubProjeto descrição");
-        
-        
+
+        Project project1 = new Project();
+        project1.setId(0);
+        project1.setDescription("Projeto teste 1");
+        projects.add(project1);
+
+        Project project2 = new Project();
+        project2.setId(1);
+        project2.setDescription("Projeto teste 2");
+        projects.add(project2);
+
+        Project project3 = new Project();
+        project3.setId(2);
+        project3.setDescription("Projeto teste 3");
+        projects.add(project2);
+
+        SubProject subProject1 = new SubProject();
+        subProject1.setId(0);
+        subProject1.setDescription("SubProjeto teste 1");
+        subProjects.add(subProject1);
+
+        SubProject subProject2 = new SubProject();
+        subProject2.setId(1);
+        subProject2.setDescription("SubProjeto teste 2");
+        subProjects.add(subProject2);
+
+        SubProject subProject3 = new SubProject();
+        subProject3.setId(2);
+        subProject3.setDescription("SubProjeto teste 3");
+        subProjects.add(subProject3);
+
         Activity activity = new Activity();
         activity.setId(0);
         activity.setDescription("Atividade descricao");
-        activity.setProject(project);
-        activity.setSubproject(subProject);
+        activity.setProject(project1);
+        activity.setSubproject(subProject1);
         activity.setStartHour(LocalDateTime.of(2014, Month.JUNE, 12, 17, 0));
         activity.setFinishHour(LocalDateTime.of(2014, Month.JUNE, 12, 18, 0));
         activities.add(activity);
-        
-        Activity activity2 = new Activity();
-        activity2.setId(0);
-        activity2.setDescription("Atividade descricao 2");
-        activity2.setProject(project);
-        activity2.setSubproject(subProject);
-        activity2.setStartHour(LocalDateTime.of(2014, Month.JUNE, 13, 9, 0));
-        activity2.setFinishHour(LocalDateTime.of(2014, Month.JUNE, 13, 11, 0));        
-        activities.add(activity2);
-        
-    }
-    
 
-    
+        Activity activity2 = new Activity();
+        activity2.setId(1);
+        activity2.setDescription("Atividade descricao 2");
+        activity2.setProject(project2);
+        activity2.setSubproject(subProject2);
+        activity2.setStartHour(LocalDateTime.of(2014, Month.JUNE, 13, 9, 0));
+        activity2.setFinishHour(LocalDateTime.of(2014, Month.JUNE, 13, 11, 0));
+        activities.add(activity2);
+
+        Activity activity3 = new Activity();
+        activity2.setId(2);
+        activity2.setDescription("Atividade descricao 3");
+        activity2.setProject(project3);
+        activity2.setSubproject(subProject3);
+        activity2.setStartHour(LocalDateTime.of(2014, Month.JUNE, 13, 9, 0));
+        activity2.setFinishHour(LocalDateTime.of(2014, Month.JUNE, 13, 11, 0));
+        activities.add(activity2);
+
+    }
+
 }
