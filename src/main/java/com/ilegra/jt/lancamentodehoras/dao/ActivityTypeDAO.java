256a101ce6d8ceb32a489ed0e23bdc4931a021eb
alineanne <alineanne27@gmail.com>
2014-12-04 14:56:48 -0200

    refatorando activity type e implementando o option

--- src/main/java/com/ilegra/jt/lancamentodehoras/dao/ActivityTypeDAO.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/dao/ActivityTypeDAO.java
@@ -4,12 +4,21 @@ import com.ilegra.jt.lancamentodehoras.model.ActivityType;
 import com.ilegra.jt.lancamentodehoras.model.User;
 import com.ilegra.jt.lancamentodehoras.repository.ActivityTypeRepository;
 import java.util.List;
+import java.util.Optional;
 
 public class ActivityTypeDAO implements ActivityTypeRepository {
    
     @Override
     public List<ActivityType> listByActivity(User user, ActivityType activityType){
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public Optional<ActivityType> getById(int id){
+        
+        return Memory.activityType
+                .stream().filter((activityType) -> id == activityType.getId())
+                .findFirst();
     }
     
     @Override
