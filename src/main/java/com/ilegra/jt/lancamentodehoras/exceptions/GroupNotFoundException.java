1b04ce3b2d50c26a34b61d353673c9be9dff9a5f
PauloMonteiro02 <paulo_monteiro@ilegra.com>
2014-12-04 14:30:19 -0200

    groupDAO

---
+++ src/main/java/com/ilegra/jt/lancamentodehoras/exceptions/GroupNotFoundException.java
@@ -0,0 +1,10 @@
+
+package com.ilegra.jt.lancamentodehoras.exceptions;
+
+public class GroupNotFoundException extends Exception {
+
+    public GroupNotFoundException() {
+        super("Group not found");
+    }
+
+}
