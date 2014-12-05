8a9142403f58c93f49bddc1d807b4c2931f77b48
PauloMonteiro01 <paulo.monteiro@ilegra.com>
2014-12-04 23:20:59 -0200

    Adicionando a Validação de Datas

--- src/main/java/com/ilegra/jt/lancamentodehoras/dao/ActivityDAO.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/dao/ActivityDAO.java
@@ -1,7 +1,6 @@
 package com.ilegra.jt.lancamentodehoras.dao;
 
 import com.ilegra.jt.lancamentodehoras.model.Activity;
-import com.ilegra.jt.lancamentodehoras.model.Project;
 import com.ilegra.jt.lancamentodehoras.model.User;
 import com.ilegra.jt.lancamentodehoras.repository.ActivityRepository;
 import java.util.List;
@@ -9,21 +8,20 @@ import java.util.List;
 public class ActivityDAO implements ActivityRepository {
 
     @Override
-    public Long add(User user, Project project, Activity activity) {
-        activity.setProject(project);
+    public Long add(User user, Activity activity) {
         activity.setUser(user);
         Memory.activities.add(activity);
         System.out.println(activity);
-        return new Long(123);
+        return (long) 123;
     }
 
     @Override
-    public void delete(User user, Project project, Activity activity) {
+    public void delete(User user, Activity activity) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void update(User user, Project project, Activity activity) {
+    public void update(User user, Activity activity) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
