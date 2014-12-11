9ae1192fee00bbe49f8feb714850b1856116ff3c
alineanne <alineanne27@gmail.com>
2014-12-11 16:57:31 -0200

    Diminuindo o número de variáveis inúteis

--- src/main/java/com/ilegra/jt/lancamentodehoras/validators/DateHelper.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/validators/DateHelper.java
@@ -8,26 +8,21 @@ import java.time.format.DateTimeFormatter;
 public class DateHelper {
 
     private static final LocalDateTime today = LocalDateTime.now();
-    private static final int daysLimit = 5;
+    private static final int LIMIT_DAYS = 5;
 
     public static LocalDateTime toLocalDateTime(String date, String hour) {
-        LocalTime time = LocalTime.parse(hour);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate localDate = LocalDate.parse(date, formatter);
-        return LocalDateTime.of(localDate, time);
+        return LocalDateTime.of(LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy")), LocalTime.parse(hour));
     }
 
     public static boolean validateDate(String date) {
-        boolean isValidate = !isEmpty(date) && !isNull(date);
-        LocalDate localDate = null;
-        if (isValidate) {
+        if (isEmpty(date) && !isNull(date)) {
             try {
-                localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
             } catch (java.time.format.DateTimeParseException exceptionMessage) {
-                isValidate = false;
+                return true;
             }
         }
-        return isValidate;
+        return false;
     }
 
     public static boolean isNull(String data) {
@@ -39,8 +34,7 @@ public class DateHelper {
     }
 
     public static boolean validateStartDateBeforeToday(LocalDateTime startHour) {
-        LocalDateTime dataLimit = today.minusDays(daysLimit);
-        return startHour.isAfter(dataLimit);
+        return startHour.isAfter(today.minusDays(LIMIT_DAYS));
     }
 
     public static boolean validateStartDateAfterToday(LocalDateTime startHour) {
