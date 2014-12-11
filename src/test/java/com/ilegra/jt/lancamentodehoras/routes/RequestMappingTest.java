abbdc2d1e0c83362d0c8e632af5e1054956d4ad7
MarceloIlegra <marcelo.serpa@ilegra.com>
2014-12-11 16:44:56 -0200

    Code review negado: Passar toda a lógica de extração de dados do request para a classe RequestMapping

--- src/test/java/com/ilegra/jt/lancamentodehoras/routes/RequestMappingTest.java
+++ src/test/java/com/ilegra/jt/lancamentodehoras/routes/RequestMappingTest.java
@@ -5,43 +5,56 @@
  */
 package com.ilegra.jt.lancamentodehoras.routes;
 
+import com.ilegra.jt.lancamentodehoras.dao.ActivityTypeDAO;
+import com.ilegra.jt.lancamentodehoras.dao.GroupDAO;
+import com.ilegra.jt.lancamentodehoras.dao.Memory;
+import com.ilegra.jt.lancamentodehoras.dao.ProjectDAO;
+import com.ilegra.jt.lancamentodehoras.dao.SubProjectDAO;
 import com.ilegra.jt.lancamentodehoras.model.Activity;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.Test;
 import spark.Request;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 public class RequestMappingTest {
     
-    @Test  
-    public void simpleTest(){
-        Request request = new MyRequest();
-        Activity expected = new Activity();
-        Activity actual= new RequestMapping().mapRequestToActivity(request);
-        assertEquals(expected, actual);
+    @Before
+    public void setup(){
+        Memory.start();
     }
+    
     @Test
-    public void mappingTest(){        
+    public void mappingTest(){           
+        assertEquals(activityFake(), new RequestMapping().mapRequestToActivity(new MyRequest(requestFakeParams())));        
+    }
+
+    private Activity activityFake() {
+        Activity expected = new Activity();
+        expected.setStartHour(LocalDateTime.of(2014, Month.DECEMBER, 20, 12, 0));
+        expected.setFinishHour(LocalDateTime.of(2014, Month.DECEMBER, 20, 16, 0));
+        expected.setProject(new ProjectDAO().getById(1).get());
+        expected.setSubProject(new SubProjectDAO().getById(1).get());
+        expected.setGroup(new GroupDAO().getById(1).get());                
+        expected.setActivityType(new ActivityTypeDAO().getById(1).get());
+        expected.setDescription("TESTE");
+        return expected;        
+    }
+
+    private Map<String, String> requestFakeParams() {
         Map<String,String> params = new HashMap<String,String>();
         params.put("data", "20/12/2014");
         params.put("horainicio","12:00");
         params.put("horafim", "16:00");
-        params.put("projeto","Projeto 1");
-        params.put("subprojeto","SubProjeto 1");
-        params.put("grupo","Grupo 1");
-        params.put("tipo_atividade","Atividade 1");
+        params.put("projeto","1");
+        params.put("subprojeto","1");
+        params.put("grupo","1");
+        params.put("tipo_atividade","1");
         params.put("descricao","TESTE");
-        Activity expected = new Activity();
-        Request request = new MyRequest(params);
-        Activity actual= new RequestMapping().mapRequestToActivity(request);
-        expected.setDescription("TESTE");
-        assertEquals(expected.getDescription(),actual.getDescription());
-        
-        
-        
-        
-        
+        return params;
     }
     
     
