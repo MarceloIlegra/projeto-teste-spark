2c0c81de3c253342a99dfb27bb1719babad778b4
MarceloIlegra <marcelo.serpa@ilegra.com>
2014-12-15 09:40:58 -0200

    Editar Atividade - Criar evento javascript nos botões de edição para preencher modal quando for clicado no botão.

---
+++ src/main/resources/public/static/js/activity-form.js
@@ -0,0 +1,47 @@
+var openModal = function(){
+    $("#novaAtividadeModal").modal("show"); 
+};
+
+var clearModal = function(){
+  $("#formulario_nova_atividade").find("textarea").val("");
+  $("#formulario_nova_atividade").find("input").not("input[type=submit]").val("");
+};
+
+var showModalEditMode = function(activity){
+  $("#formulario_nova_atividade").prop("method", "put"); 
+  prepareModal(activity);
+  openModal();
+};  
+
+var showModalNewMode = function(){
+  $("#formulario_nova_atividade").prop("method", "post"); 
+  clearModal();
+}; 
+
+var prepareModal = function(activity){
+    var date = activity.startHour.date;
+    $("#nova-atividade-data").val(date.day+"/"+date.month+"/"+date.year);
+    $("#nova-atividade-horainicio").val(activity.startHour.time.hour+":"+activity.startHour.time.minute);
+    $("#nova-atividade-horafim").val(activity.finishHour.time.hour+":"+activity.finishHour.time.minute);
+    $("#nova-atividade-projeto").val(activity.project.id);
+    $("#nova-atividade-subprojeto").val(activity.subProject.id);
+    $("#nova-atividade-grupo").val(activity.group.id);
+    $("#nova-atividade-tipo").val(activity.activityType.id);
+    $("#nova-atividade-descricao").val(activity.description);
+    $("#nova-atividade-id").val(activity.id);
+};
+
+$(document).ready(function(){
+    $(".editar_atividade").click(function(event){
+       event.preventDefault();
+       $.getJSON($(this).attr("href"), function(data){                    
+          showModalEditMode(data);                    
+          console.log(data);
+       });
+    });
+    $("#abrirModalAtividade").click(function(event){
+      event.preventDefault();
+      showModalNewMode();
+    });
+
+});
