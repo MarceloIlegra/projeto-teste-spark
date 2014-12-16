966713871d4bd13105bff48c28aa5114cbf265e0
alineanne <alineanne27@gmail.com>
2014-12-16 16:04:51 -0200

    refatorando o requestmapping e o teste

--- src/main/java/com/ilegra/jt/lancamentodehoras/routes/RequestMapping.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/routes/RequestMapping.java
@@ -4,24 +4,27 @@ import com.ilegra.jt.lancamentodehoras.dao.ActivityTypeDAO;
 import com.ilegra.jt.lancamentodehoras.dao.GroupDAO;
 import com.ilegra.jt.lancamentodehoras.dao.ProjectDAO;
 import com.ilegra.jt.lancamentodehoras.dao.SubProjectDAO;
+import com.ilegra.jt.lancamentodehoras.dao.UserDAO;
 import com.ilegra.jt.lancamentodehoras.pojo.Activity;
 import static com.ilegra.jt.lancamentodehoras.validators.RequestValidator.toLocalDateTime;
 import spark.Request;
 
 public class RequestMapping {
+    Activity activity = new Activity();
     
-    public Activity  mapRequestToActivity(Request request){               
-        Activity activity = new Activity();
+    public Activity  mapRequestToActivity(Request request){          
+        loadActivity(request);        
+        return activity;
+    }
+    
+    private void loadActivity(Request request) {
+        activity.setUser(new UserDAO().login(request.queryParams("usuario"),request.queryParams("senha")).get());
         activity.setStartHour(toLocalDateTime(request.queryParams("data"), request.queryParams("horainicio")));
         activity.setFinishHour(toLocalDateTime(request.queryParams("data"), request.queryParams("horafim")));
         activity.setProject(new ProjectDAO().getById(new Integer(request.queryParams("projeto"))).get());
         activity.setSubProject(new SubProjectDAO().getById(new Integer(request.queryParams("subprojeto"))).get());
         activity.setGroup(new GroupDAO().getById(new Integer(request.queryParams("grupo"))).get());
         activity.setActivityType(new ActivityTypeDAO().getById(new Integer(request.queryParams("tipo_atividade"))).get());
-        activity.setDescription(request.queryParams("descricao")); 
-        if(request.params(":id") != null && !request.params(":id").equals("")){
-            activity.setId(new Long(request.params(":id")));
-        }
-        return activity;
+        activity.setDescription(request.queryParams("descricao"));
     }    
 }
--- src/test/java/com/ilegra/jt/lancamentodehoras/routes/RequestMappingTest.java
+++ src/test/java/com/ilegra/jt/lancamentodehoras/routes/RequestMappingTest.java
@@ -1,4 +1,3 @@
-
 package com.ilegra.jt.lancamentodehoras.routes;
 
 import com.ilegra.jt.lancamentodehoras.dao.ActivityTypeDAO;
@@ -6,6 +5,7 @@ import com.ilegra.jt.lancamentodehoras.dao.GroupDAO;
 import com.ilegra.jt.lancamentodehoras.dao.Memory;
 import com.ilegra.jt.lancamentodehoras.dao.ProjectDAO;
 import com.ilegra.jt.lancamentodehoras.dao.SubProjectDAO;
+import com.ilegra.jt.lancamentodehoras.dao.UserDAO;
 import com.ilegra.jt.lancamentodehoras.pojo.Activity;
 import java.time.LocalDateTime;
 import java.time.Month;
@@ -29,6 +29,7 @@ public class RequestMappingTest {
 
     private Activity activityFake() {
         Activity expected = new Activity();
+        expected.setUser(new UserDAO().login("aline","123").get());
         expected.setStartHour(LocalDateTime.of(2014, Month.DECEMBER, 20, 12, 0));
         expected.setFinishHour(LocalDateTime.of(2014, Month.DECEMBER, 20, 16, 0));
         expected.setProject(new ProjectDAO().getById(1).get());
@@ -41,6 +42,8 @@ public class RequestMappingTest {
 
     private Map<String, String> requestFakeParams() {
         Map<String,String> params = new HashMap<>();
+        params.put("usuario","aline");
+        params.put("senha","123");
         params.put("data", "20/12/2014");
         params.put("horainicio","12:00");
         params.put("horafim", "16:00");
