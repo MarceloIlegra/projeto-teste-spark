fe4b19e608bd0ed0ae25ba5bce6de6116e3e36a5
PauloMonteiro02 <paulo.monteiro@ilegra.com>
2015-01-15 13:13:39 -0200

    Retirando o parametro Projeto dos metodos da interface da Atividade

--- src/main/java/com/ilegra/jt/lancamentodehoras/repository/ActivityRepository.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/repository/ActivityRepository.java
@@ -1,7 +1,6 @@
 package com.ilegra.jt.lancamentodehoras.repository;
 
 import com.ilegra.jt.lancamentodehoras.pojo.Activity;
-import com.ilegra.jt.lancamentodehoras.pojo.Project;
 import com.ilegra.jt.lancamentodehoras.pojo.User;
 import java.util.List;
 import java.util.Optional;
@@ -10,13 +9,13 @@ public interface ActivityRepository {
 
     public Long add(User user, Activity activity);
 
-    public void delete(User user, Project project, Activity activity);
+    public void delete(User user,Activity activity);
 
-    public void update(User user, Project project, Activity activity);
+    public void update(User user,Activity activity);
     
     public Optional<Activity> find(Long id);
 
-    public List<Activity> listByMonth(User user,Project project, Short month);
+    public List<Activity> listByMonth(User user,Short month);
 
     public List<Activity> listAll();
     
