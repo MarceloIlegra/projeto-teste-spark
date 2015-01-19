f07f317b19c38d10ed84de750982ca335b7388ff
PauloMonteiro02 <paulo.monteiro@ilegra.com>
2015-01-19 13:45:28 -0200

    Refatorando a Interface Activity

--- src/main/java/com/ilegra/jt/lancamentodehoras/repository/ActivityRepository.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/repository/ActivityRepository.java
@@ -1,21 +1,20 @@
 package com.ilegra.jt.lancamentodehoras.repository;
 
 import com.ilegra.jt.lancamentodehoras.pojo.Activity;
-import com.ilegra.jt.lancamentodehoras.pojo.User;
 import java.util.List;
 import java.util.Optional;
 
 public interface ActivityRepository {
 
-    public Long add(User user, Activity activity);
+    public Long add(Activity activity);
 
-    public void delete(User user,Activity activity);
+    public void delete(Activity activity);
 
-    public void update(User user,Activity activity);
+    public void update(Activity activity);
     
     public Optional<Activity> find(Long id);
 
-    public List<Activity> listByMonth(User user,Short month);
+    public List<Activity> listByMonth(Short month);
 
     public List<Activity> listAll();
 }
