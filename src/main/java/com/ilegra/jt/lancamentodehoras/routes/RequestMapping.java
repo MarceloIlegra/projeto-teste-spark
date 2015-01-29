783061d84f713172e2ca9e9391814b163f4ff119
PauloMonteiro02 <paulo.monteiro@ilegra.com>
2015-01-29 13:49:58 -0200

    Alterando o retorno dos metodos requestTo...

--- src/main/java/com/ilegra/jt/lancamentodehoras/routes/RequestMapping.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/routes/RequestMapping.java
@@ -10,6 +10,7 @@ import com.ilegra.jt.lancamentodehoras.service.GroupService;
 import com.ilegra.jt.lancamentodehoras.service.ProjectService;
 import com.ilegra.jt.lancamentodehoras.service.SubProjectService;
 import static com.ilegra.jt.lancamentodehoras.validators.RequestValidator.toLocalDateTime;
+import java.util.Optional;
 import spark.Request;
 
 public class RequestMapping {
@@ -20,10 +21,10 @@ public class RequestMapping {
         activity.setUser(request.session().attribute("login"));
         activity.setStartHour(toLocalDateTime(request.queryParams("data"), request.queryParams("horainicio")));
         activity.setFinishHour(toLocalDateTime(request.queryParams("data"), request.queryParams("horafim")));
-        activity.setProject((requestToProject(request)));
-        activity.setSubProject((requestToSubProject(request)));
-        activity.setGroup((requestToGroup(request)));
-        activity.setActivityType((requestToActivityType(request)));      
+        activity.setProject(ProjectService.getById(new Integer(request.queryParams("projeto"))).get());
+        activity.setSubProject(SubProjectService.getById(new Integer(request.queryParams("subprojeto"))).get());
+        activity.setGroup(GroupService.getById(new Integer(request.queryParams("grupo"))).get());
+        activity.setActivityType(ActivityTypeService.getById(new Integer(request.queryParams("tipo_atividade"))).get());
         activity.setDescription(request.queryParams("descricao"));       
         if(request.queryParams("nova-atividade-id") !=null && !request.queryParams("nova-atividade-id").equals("")){
             activity.setId(new Long(request.queryParams("nova-atividade-id")));             
@@ -35,22 +36,22 @@ public class RequestMapping {
         return Short.parseShort(request.queryParams("mes_ano"));
     }
     
-    public Project requestToProject(Request request){
+    public Optional<Project> requestToProject(Request request){
         ProjectService projectService = new ProjectService();
         return projectService.getById(new Integer(request.queryParams("projeto")));
     }
     
-    public SubProject requestToSubProject(Request request){
+    public Optional<SubProject> requestToSubProject(Request request){
         SubProjectService subProjectService = new SubProjectService();
         return subProjectService.getById(new Integer(request.queryParams("subprojeto")));
     }
     
-    public Group requestToGroup(Request request){
+    public Optional<Group> requestToGroup(Request request){
         GroupService groupService = new GroupService();
         return groupService.getById(new Integer(request.queryParams("grupo")));
     }
     
-    public ActivityType requestToActivityType(Request request){
+    public Optional<ActivityType> requestToActivityType(Request request){
         ActivityTypeService activityTypeService = new ActivityTypeService();
         return activityTypeService.getById(new Integer(request.queryParams("tipo_atividade")));
     }
