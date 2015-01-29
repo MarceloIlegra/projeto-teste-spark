16bcce80fbe8a3e9bb946d3bbbd062417c2b5bb5
PauloMonteiro02 <paulo.monteiro@ilegra.com>
2015-01-29 13:47:48 -0200

    Alterando o retorno do  metodo getById da classe SubProjectService para  Optional<SubProject>

--- src/main/java/com/ilegra/jt/lancamentodehoras/service/SubProjectService.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/service/SubProjectService.java
@@ -2,11 +2,12 @@ package com.ilegra.jt.lancamentodehoras.service;
 
 import com.ilegra.jt.lancamentodehoras.dao.SubProjectDAO;
 import com.ilegra.jt.lancamentodehoras.pojo.SubProject;
+import java.util.Optional;
 
 public class SubProjectService {
     
-    public static SubProject getById(Integer id) {
-        return new SubProjectDAO().getById(id).get();
+    public static Optional<SubProject> getById(Integer id) {
+        return new SubProjectDAO().getById(id);
     }
     
 }
