5c16ab3e06a0187bcf9a4c9c55d9f68e4d308b4a
PauloMonteiro02 <paulo.monteiro@ilegra.com>
2015-01-29 13:47:01 -0200

    Alterando o retorno do  metodo getById da classe GroupService para  Optional<Group>

--- src/main/java/com/ilegra/jt/lancamentodehoras/service/GroupService.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/service/GroupService.java
@@ -2,10 +2,11 @@ package com.ilegra.jt.lancamentodehoras.service;
 
 import com.ilegra.jt.lancamentodehoras.dao.GroupDAO;
 import com.ilegra.jt.lancamentodehoras.pojo.Group;
+import java.util.Optional;
 
 public class GroupService {
     
-    public static Group getById(Integer id) {
-        return new GroupDAO().getById(id).get();
+    public static Optional<Group> getById(Integer id) {
+        return new GroupDAO().getById(id);
     }    
 }
