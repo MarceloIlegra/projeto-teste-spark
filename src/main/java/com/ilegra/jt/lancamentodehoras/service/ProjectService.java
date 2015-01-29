0210ea5987d5e0e649df5a9ad65c412e63b29ed5
PauloMonteiro02 <paulo.monteiro@ilegra.com>
2015-01-29 13:43:01 -0200

    Alterando o metodo getById de Project para Optional<Project>

--- src/main/java/com/ilegra/jt/lancamentodehoras/service/ProjectService.java
+++ src/main/java/com/ilegra/jt/lancamentodehoras/service/ProjectService.java
@@ -2,10 +2,11 @@ package com.ilegra.jt.lancamentodehoras.service;
 
 import com.ilegra.jt.lancamentodehoras.dao.ProjectDAO;
 import com.ilegra.jt.lancamentodehoras.pojo.Project;
+import java.util.Optional;
 
 public class ProjectService {
 
-    public static Project getById(Integer id) {
-        return new ProjectDAO().getById(id).get();
+    public static Optional<Project> getById(Integer id) {
+        return new ProjectDAO().getById(id);
     }
 }
